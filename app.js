b6f55b50af713b809c4e59a7d287f9f421c17257
Nikolaos IOANNOU <nikolaos.ioannou.e@thalesdigital.io>
2020-03-18 17:17:24 +0100

    add https module and use it to get a url

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express")
 
+const https = require("https");
+
 const app = express();
 
 
 app.get("/", function(req,res){
-
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Athens&appid=4a20317e712c6b1a261f29900e05869e&units=metric"
+    
+    https.get(url, function(response){
+        console.log(response)
+    })
+    
+    
     res.send("Server runs")
+
 })
 
 app.listen(3000, function(req,res){
