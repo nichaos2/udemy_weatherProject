43a75663049790d7d5cb9f48aa63b6eccfa9e62f
Nikolaos IOANNOU <nikolaos.ioannou.e@thalesdigital.io>
2020-05-04 16:10:20 +0200

    add body-parser package and use

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser")
+
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html")
@@ -11,7 +14,6 @@ app.post("/", function (req, res) {
     "https://api.openweathermap.org/data/2.5/weather?q=Athens&appid=4a20317e712c6b1a261f29900e05869e&units=metric";
 
   https.get(url, function (response) {
-    console.log(response);
     // log the status
     console.log(response.statusCode);
 
@@ -27,15 +29,15 @@ app.post("/", function (req, res) {
       console.log("Temperature in Athens is: " + temp);
       console.log("It is: " + desc);
 
-        // get the weather icon
-        const icon_url_start =  "http://openweathermap.org/img/wn/"; 
-        const icon_url_end =  "@2x.png"; 
-        const icon_url_data =  weatherData.weather[0].icon; 
-        const icon_url = icon_url_start + icon_url_data + icon_url_end;
+    // get the weather icon
+    const icon_url_start =  "http://openweathermap.org/img/wn/"; 
+    const icon_url_end =  "@2x.png"; 
+    const icon_url_data =  weatherData.weather[0].icon; 
+    const icon_url = icon_url_start + icon_url_data + icon_url_end;
 
-      res.send( "<h3> Temperature in Athens is: " + temp + "</h3>" +
-                "<h4> The weather is : " + desc + "</h4> "+
-                '<br> <img src="' + icon_url +'" alt="Weather in Athens" height="120">' );
+    res.send( "<h3> Temperature in Athens is: " + temp + "</h3>" +
+            "<h4> The weather is : " + desc + "</h4> "+
+            '<br> <img src="' + icon_url +'" alt="Weather in Athens" height="120">' );
     });
   });
 
