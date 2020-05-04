686941d157179b7c39f9ec8f3f3c627188db5248
Nikolaos IOANNOU <nikolaos.ioannou.e@thalesdigital.io>
2020-05-04 16:19:40 +0200

    add query cityName in the post action

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const https = require("https");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req,res){
-    res.sendFile(__dirname + "/index.html")
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.post("/", function (req, res) {
+  const cityName = req.body.cityName;
+  const appid = "4a20317e712c6b1a261f29900e05869e";
+
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Athens&appid=4a20317e712c6b1a261f29900e05869e&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" + appid +
+    "&units=metric";
 
   https.get(url, function (response) {
     // log the status
@@ -29,15 +35,23 @@ app.post("/", function (req, res) {
       console.log("Temperature in Athens is: " + temp);
       console.log("It is: " + desc);
 
-    // get the weather icon
-    const icon_url_start =  "http://openweathermap.org/img/wn/"; 
-    const icon_url_end =  "@2x.png"; 
-    const icon_url_data =  weatherData.weather[0].icon; 
-    const icon_url = icon_url_start + icon_url_data + icon_url_end;
+      // get the weather icon
+      const icon_url_start = "http://openweathermap.org/img/wn/";
+      const icon_url_end = "@2x.png";
+      const icon_url_data = weatherData.weather[0].icon;
+      const icon_url = icon_url_start + icon_url_data + icon_url_end;
 
-    res.send( "<h3> Temperature in Athens is: " + temp + "</h3>" +
-            "<h4> The weather is : " + desc + "</h4> "+
-            '<br> <img src="' + icon_url +'" alt="Weather in Athens" height="120">' );
+      res.send(
+        "<h3> Temperature in " + cityName +" is: " +
+          temp +
+          "</h3>" +
+          "<h4> The weather is : " +
+          desc +
+          "</h4> " +
+          '<br> <img src="' +
+          icon_url +
+          '" alt="Weather Icon" height="120">'
+      );
     });
   });
 
@@ -45,7 +59,7 @@ app.post("/", function (req, res) {
   //res.send("Server runs")
 });
 
-// 
+//
 app.listen(3000, function (req, res) {
   console.log("Port listen 3000");
 });
